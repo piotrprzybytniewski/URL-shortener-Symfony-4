e05fcabbc78e55f90653d07429de1dc488a2864b
selfmadeking <piotr.przybytniewski@gmail.com>
2018-08-08 18:51:39 +0200

    Added service which increments clicks in a given link.

---
+++ src/Controller/Redirect/SingleUrlRedirectController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Controller\Redirect;
+
+
+use App\Repository\UrlRepository;
+use App\Service\UrlStatisticsService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SingleUrlRedirectController extends AbstractController
+{
+    /**
+     * @Route("/{shortenedUrl}", methods={"GET"}, name="single_url_redirect")
+     */
+    public function singleUrlRedirect($shortenedUrl, UrlRepository $urlRepository, UrlStatisticsService $urlStatisticsService)
+    {
+        $url = $urlRepository->findOneBy([
+            'shortenedUrl' => $shortenedUrl,
+        ]);
+
+        $id = $url->getId();
+        $originalUrl = $url->getOriginalUrl();
+
+        $urlStatisticsService->updateStatistics($id);
+
+
+        return $this->redirect($originalUrl);
+    }
+}
--- src/Entity/Url.php
+++ src/Entity/Url.php
@@ -114,4 +114,6 @@ class Url
 
         return $this;
     }
+
+
 }
